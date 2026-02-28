e2fcbef907f2f8da3370e71b9739d334274cc747
giuseppe.tegano <giuseppe.tegano@lynxspa.com>
2026-02-28 13:45:35 +0100

    initialize CRUD for DeviceService
    - add basic CRUD for almost every method

--- src/main/java/com/giuseppe/devices/service/DeviceService.java
+++ src/main/java/com/giuseppe/devices/service/DeviceService.java
@@ -1,11 +1,14 @@
 package com.giuseppe.devices.service;
 
+import com.giuseppe.devices.domain.Device;
 import com.giuseppe.devices.dto.DeviceRequest;
 import com.giuseppe.devices.dto.DeviceResponse;
+import com.giuseppe.devices.mapper.DeviceMapper;
 import com.giuseppe.devices.repository.DeviceRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DeviceService {
@@ -17,29 +20,50 @@ public class DeviceService {
     }
 
     public DeviceResponse create(DeviceRequest request) {
-        return null;
+        Device device = DeviceMapper.requestToEntity(request);
+        Device saved = deviceRepository.save(device);
+        return DeviceMapper.entityToResponse(saved);
     }
 
     public DeviceResponse update(String id, DeviceRequest request) {
+        Optional<Device> deviceOptional = deviceRepository.findById(id);
+
+        if (deviceOptional.isPresent()) {
+            Device device = deviceOptional.get();
+            device.setName(request.name());
+            device.setBrand(request.brand());
+            device.setState(request.state());
+
+            Device updated = deviceRepository.save(device);
+            return DeviceMapper.entityToResponse(updated);
+        }
+
         return null;
     }
 
     public DeviceResponse getById(String id) {
-        return null;
+        Optional<Device> deviceOptional = deviceRepository.findById(id);
+        return deviceOptional.map(DeviceMapper::entityToResponse).orElse(null);
     }
 
+    // Not sure if it should work alone
     public DeviceResponse getByBrand(String id) {
         return null;
     }
 
+    // Not sure if it should work alone
     public DeviceResponse getByState(String id) {
         return null;
     }
 
+    // With get all I can probably manage getByBrand and getByState in one shot
     public List<DeviceResponse> getAll() {
-        return null;
+        List<Device> byFilters = deviceRepository.findAll();
+        return byFilters.stream().map(DeviceMapper::entityToResponse).toList();
     }
 
     public void delete(String id) {
+        Optional<Device> deviceOptional = deviceRepository.findById(id);
+        deviceOptional.ifPresent(deviceRepository::delete);
     }
 }
