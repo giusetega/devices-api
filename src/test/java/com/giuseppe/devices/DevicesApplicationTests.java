48de4c04646d4ec57722c30594fc23c2afa891f4
giuseppe.tegano <giuseppe.tegano@lynxspa.com>
2026-02-28 11:25:17 +0100

    initialize Spring Boot project

---
+++ src/test/java/com/giuseppe/devices/DevicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giuseppe.devices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
